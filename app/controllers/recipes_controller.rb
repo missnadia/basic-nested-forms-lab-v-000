a810cee57f6f6e6866c21d7e844e209f1a1a6378
Nadia Lee <miss.nadia.lee@gmail.com>
2018-06-12 23:47:38 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,39 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build(recipe_params)
+    @recipe.ingredients.build(recipe_params)
   end
 
   def create
+    @recipe = Recipe.create(recipe_params)
+      redirect_to recipe_path(@recipe)
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(recipe_params)
+      redirect_to recipe_path(@recipe)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Recipe.find(params[:id]).destroy
+    redirect_to recipes_url
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(
+      :title,
+      ingredients_attributes: [
+        :id,
+        :name,
+        :quantity
+      ]
+    )
   end
 end
